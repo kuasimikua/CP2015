a6fc7fcec4b7489d7290b4a1359a75b5b1008deb
kuasimikua <kua.wengchew.ernest@dhs.sg>
2015-02-01 23:31:56 +0800

    Create q2_calc_cylinder_volume.py

---
+++ P01/q2_calc_cylinder_volume.py
@@ -0,0 +1,5 @@
+radius=float(input("Please enter the radius of the cylinder (in cm): "))
+length=float(input("Please enter the length of the cylinder (in cm): "))
+area=radius*radius*3.1415926535
+volume=float(area)*length
+print("The volume of the cylinder is approximately "+ str(volume) +"cm^3")
