3a29bcf5b51fadc6a1f193c14081d6dab4957651
kuasimikua <kua.wengchew.ernest@dhs.sg>
2015-02-02 10:11:42 +0800

    Create q5_upper_to_lower.py

---
+++ P01/q5_upper_to_lower.py
@@ -0,0 +1,10 @@
+upper=input("Please enter an uppercase letter: ")
+if len(upper)>1:
+    print("One uppercase character only! :(")
+elif upper==upper.lower():
+    print("One uppercase character only! :(")
+elif upper.isalpha:
+    lower=(ord(upper))+32
+    print(chr(lower))
+else:
+    print("One uppercase character only! :(")
