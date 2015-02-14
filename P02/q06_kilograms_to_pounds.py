29d17037d72afc658118f91b0dbd2a51de88b5c9
kuasimikua <kua.wengchew.ernest@dhs.sg>
2015-02-14 22:09:44 +0800

    Update q06_kilograms_to_pounds.py

--- P02/q06_kilograms_to_pounds.py
+++ P02/q06_kilograms_to_pounds.py
@@ -2,4 +2,4 @@ kilograms= "Kilograms"
 pounds = "Pounds"
 print('{0:10} {1}').format(kilograms, pounds)
 for i in range(11):
-    print("{0:<9} {1:.1f}".format(i,i*2.2))
+    print("{0:<10} {1:.1f}".format(i,i*2.2))
